70ecd82056920f7053c16c270a22a128ffc43a6a
Elias Sjögreen <elias@5monkeys.se>
2024-07-07 23:56:10 +0200

    feat: write a little introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,28 @@ import { Layout } from "components/Layout.tsx";
 function Index() {
   return (
     <Layout>
-      Hello, World!
+      <h1>Welcome!</h1>
+      <p>
+        Nova is a ECMAScript and WASM engine written in Rust and following{" "}
+        <a href="https://en.wikipedia.org/wiki/Data-oriented_design">
+          data-oriented design principles
+        </a>. It is currently nothing more than a fun experiment to learn and to
+        prove the viability of such an engine, but may very well become
+        something much more in the future.
+      </p>
+      <p>
+        The engine is still very limited in it's capabilities only passing about
+        {" "}
+        <a href="https://test262.fyi/#|nova">
+          25% of the test262 test suite
+        </a>.{" "}
+        However development is ongoing and we are quickly improving the engine.
+        If you are interested in the project, please check out the{" "}
+        <a href="https://github.com/trynova/nova">GitHub repository</a>{" "}
+        and or join our{" "}
+        <a href="https://discord.gg/RTrgJzXKUM">Discord server</a>{" "}
+        where the core team resides and where we discuss development.
+      </p>
     </Layout>
   );
 }
