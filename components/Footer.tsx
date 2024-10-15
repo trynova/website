efa5593dc98a0a5f863ed1a0b33eeed90620aecf
Elias Sjögreen <elias@5monkeys.se>
2024-10-15 19:50:10 +0200

    fix: Space between and icon size in footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ import { css } from "utils/css.ts";
 import { CopyLeft } from "components/CopyLeft.tsx";
 import { GithubIcon } from "components/icons/Github.tsx";
 import { DiscordIcon } from "components/icons/Discord.tsx";
+
 const classes = await css(import.meta.resolve("./Footer.css"));
 
 export function Footer() {
@@ -11,14 +12,14 @@ export function Footer() {
       <a href="https://github.com/trynova">
         <CopyLeft /> {new Date().getFullYear()} The Nova Contributors
       </a>
-      <div class={classes.links}>
-        <a href="https://github.com/trynova/nova">
-          <GithubIcon width="2em" height="2em" />
+      <section class={classes.links}>
+        <a aria-label="GitHub" href="https://github.com/trynova/nova">
+          <GithubIcon />
         </a>
-        <a href="https://discord.gg/RTrgJzXKUM">
-          <DiscordIcon width="2em" height="2em" />
+        <a aria-label="Discord" href="https://discord.gg/RTrgJzXKUM">
+          <DiscordIcon />
         </a>
-      </div>
+      </section>
     </footer>
   );
 }
