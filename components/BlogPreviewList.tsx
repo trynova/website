7957fcb6b3493ed289f06e97137b13e9cb5886c3
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-12-05 14:28:21 -0500

    feat: Display latest posts on start and add bluesky social (#31)
    Co-authored-by: Elias Sjögreen <elias@5monkeys.se>

---
+++ components/BlogPreviewList.tsx
@@ -0,0 +1,24 @@
+import { posts } from "../pages/blog/utils.ts";
+import { css } from "utils/css.ts";
+import { href } from "utils/path.ts";
+
+const classes = await css(import.meta.resolve("./BlogPreviewList.css"));
+const allPosts = (await Array.fromAsync(posts()))
+  .sort(({ meta: { date: a } }, { meta: { date: b } }) =>
+    Temporal.PlainDate.compare(b, a)
+  );
+
+export function BlogPreviewList() {
+  return (
+    <ul class={classes.list}>
+      {allPosts.slice(0, 5).map((post) => (
+        <div class={classes.post}>
+          <a href={href(post.file)}>{post.meta.title}</a>
+          <time datetime={post.meta.date.toString()} class={classes.meta}>
+            {post.meta.date.toLocaleString("sv-SE")}
+          </time>
+        </div>
+      ))}
+    </ul>
+  );
+}
