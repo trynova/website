eb08b75765ae4411fa1b1bd16a344b300550ddc0
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-05-09 00:37:54 +0300

    feat: add marked-footnote and update marked (#45)

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,6 @@
 import { renderToString } from "preact-render-to-string";
 import { Feed } from "feed";
-import * as marked from "marked";
+import { Marked } from "marked";
 
 import { html } from "utils/html.ts";
 import { absoluteHref, output } from "utils/path.ts";
@@ -15,18 +15,17 @@ import { css } from "utils/css.ts";
 
 const classes = await css(import.meta.resolve("./index.css"));
 
-const allPosts = (await Array.fromAsync(posts()))
-  .sort(({ meta: { date: a } }, { meta: { date: b } }) =>
-    Temporal.PlainDate.compare(b, a)
-  );
+const allPosts = (await Array.fromAsync(posts())).sort(
+  ({ meta: { date: a } }, { meta: { date: b } }) =>
+    Temporal.PlainDate.compare(b, a),
+);
 
 function Index() {
   return (
     <Layout>
       <h1>Blog</h1>
       <ul class={classes.list}>
-        {allPosts
-          .map((post) => <BlogPostCard {...post} />)}
+        {allPosts.map((post) => <BlogPostCard {...post} />)}
       </ul>
     </Layout>
   );
@@ -52,25 +51,22 @@ if (import.meta.main) {
     copyright: `Copyleft ${new Date().getFullYear()} The Nova Contributors`,
   });
 
-  allPosts
-    .forEach((post) => {
-      const link = absoluteHref(post.file);
+  allPosts.forEach((post) => {
+    const link = absoluteHref(post.file);
 
-      feed.addItem({
-        title: post.meta.title,
-        id: link,
-        link,
-        description: post.meta.description,
-        content: marked.parse(post.body, { async: false }) as string,
-        author: post.meta.authors.map((author) => ({
-          name: author.name,
-          link: author.url,
-        })),
-        date: new Date(
-          post.meta.date.toZonedDateTime("UTC").epochMilliseconds,
-        ),
-      });
+    feed.addItem({
+      title: post.meta.title,
+      id: link,
+      link,
+      description: post.meta.description,
+      content: new Marked().parse(post.body, { async: false }) as string,
+      author: post.meta.authors.map((author) => ({
+        name: author.name,
+        link: author.url,
+      })),
+      date: new Date(post.meta.date.toZonedDateTime("UTC").epochMilliseconds),
     });
+  });
 
   await write(output(import.meta.resolve("./feed.rss")), feed.rss2());
   await write(output(import.meta.resolve("./feed.atom")), feed.atom1());
